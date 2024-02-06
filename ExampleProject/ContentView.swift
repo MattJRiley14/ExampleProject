1f937bf64d10f18a3fc22b27b832926edf455a2d
Matt Riley <matthew.riley@barren.kyschools.us>
2024-02-06 09:06:44 -0600

    Added Party

--- ExampleProject/ContentView.swift
+++ ExampleProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Pizza!")
+            Text("Pizza Party!")
         }
         .padding()
     }
